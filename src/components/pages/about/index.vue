f5be41d27e18d58e907880212f9414447b5ce671
Brian Monsales <brianmonsales@gmail.com>
2023-08-06 06:00:07 +0700

    added vue-easy-lightbox

--- src/components/pages/about/index.vue
+++ src/components/pages/about/index.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
     import UiMenu from '@/components/common/uiMenu/index.vue'
+    import VueEasyLightbox, { useEasyLightbox } from 'vue-easy-lightbox'
 
     const menu = [
         { name: 'Skills', route: 'about-skills' },
         { name: 'Profile', route: 'about' },
         { name: 'Misc', route: 'about-misc' }
     ]
+
+    const {
+      show, onHide,
+      visibleRef, indexRef, imgsRef
+    } = useEasyLightbox({
+      imgs: [
+        '/src/assets/me_nck.jpg',
+      ],
+      initIndex: 0
+    })
+    
 </script>
 
 <template>
@@ -18,11 +30,16 @@
                 <div class="row center-xs">
                     <div class="col-xs-10 col-md-6">
                         <div class="g-avatar-con">
-                        <!-- <lightbox
-                            thumbnail="/src/assets/me_nck.jpg"
-                            :images="['/src/assets/me_nck.jpg']"
-                        ></lightbox> -->
-                        </div>
+                            <template v-for="img in imgsRef">
+                                <img :src="img" @click="show" />
+                            </template>
+                            <vue-easy-lightbox
+                                :visible="visibleRef"
+                                :imgs="imgsRef"
+                                :index="indexRef"
+                                @hide="onHide"
+                            ></vue-easy-lightbox>
+                        </div> 
                         <div class="g-menu-con">
                         <ui-menu class="g-menu" :menu="menu"></ui-menu>
                         </div>
