fef627be096f006921cc6a70c310f9581c3a8e20
Brian Monsales <brianmonsales@gmail.com>
2023-08-08 22:10:25 +0700

    updated build files

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/brickgale.github.io/',
     resolve:{
         alias:{
             '@' : path.resolve(__dirname, './src')
