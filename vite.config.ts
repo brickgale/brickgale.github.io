5b80968c53b92b1a89c6189472fc80e77d102549
Brian Monsales <brianmonsales@gmail.com>
2023-08-08 22:17:42 +0700

    updated vite config and added static.yml

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/brickgale.github.io/',
+    base: './',
     resolve:{
         alias:{
             '@' : path.resolve(__dirname, './src')
